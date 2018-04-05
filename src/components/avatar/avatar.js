3c9673a7d52a13b59d09833a26202cf76077182a
rwthoo <rwthoo@gmail.com>
2018-04-04 23:47:49 -0400

    lecture_3

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -4,6 +4,13 @@ import './avatar.css';
 export const Avatar = (props) => (
   <div className="avatar">
     <img className="avatar__img" src={props.avatarUrl} alt="avatar"/>
-    <button>Like Me</button>
-  </div>
+    <button className="like"></button>
 );
+//$('.like').click(function() {
+//    $('.unlike').hide();
+//    $('.like').show();
+//});
+//$('.unlike').click(function() {
+//    $('.like').show();
+//    $('.unlike').hide();
+//});
--- src/components/post/post.js
+++ src/components/post/post.js
@@ -4,53 +4,69 @@ import { Avatar } from '../avatar';
 import { Comment } from './comment.js';
 
 export class Post extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
     this.state = {
-      comments: this.props.comments
-    };
+      likes: 0,
+    }
+likes(){
+    this.setState({likes: this.state.likes + 1});
+  };
+
+export class Post extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      comments: props.comments,
+    }
   }
 
-  handleCommentSubmission(event){
-    event.preventDefault();
-    const text = event.target[0].value;
+handleCommentSubmission(event) {
+  // prevent page from refreshing
+  event.preventDefault();
 
-//create a comments
-const comment = {
-  owner: "rwthoo",
-  text: text,
+  // Create a comment object from the text
+  const text = event.target[0].value;
+  const comment = { owner: 'alaboudi', text: text };
+
+  // Erase the text from the input box
+  event.target[0].value = '';
+  console.log(event);
+
+  // include the new comment in the comment array
+  const comments = this.state.comments
+  comments.push(comment);
+
+  // update the state of the component with the new comment
+  this.setState({
+    comments: comments
+  });
 }
 
-// add comment and create a new state
-const comments = this.state.comments;
-comments.push(comment);
-
-//update the state
-this.setState({
-  comments: comments
-})
-
-event.target[0].value ="";
+  render(){
+    return (
+      <div className="post">
+       <div className="post__header">
+         <div className="post__avatar">
+           <Avatar avatarUrl={this.props.owner.avatarUrl} />
+         </div>
+         <div className="post__header-info">
+           <div><b>{this.props.owner.name}</b></div>
+           <div>{this.props.location}</div>
+         </div>
+       </div>
+       <img className="post__img" src={this.props.imageUrl} alt='post'/>
+       <div className="post__body">
+         <div className="post__likes"><b>{this.props.likes} likes</b></div>
+         { this.props.comments.map(comment => <Comment owner={comment.owner} text={comment.text} />) }
+         <hr className="post__body-separator" />
+         <form
+           onSubmit={(event) => this.handleCommentSubmission(event)}
+           className="post__comment-form">
+           <input placeholder="Add a comment..." className="post__comment-input"/>
+         </form>
+       </div>
+     </div>
+    );
   }
 }
-
-export const Post = (props) => (
-  <div className="post">
-    <div className="post__header">
-      <div className="post__avatar">
-        <Avatar avatarUrl={props.owner.avatarUrl} />
-      </div>
-      <div className="post__header-info">
-        <div><b>{props.owner.name}</b></div>
-        <div>{props.location}</div>
-      </div>
-    </div>
-    <img className="post__img" src={props.imageUrl} alt='post'/>
-    <div className="post__body">
-      <div className="post__likes"><b>{props.likes} likes</b></div>
-      { props.comments.map(comment => <Comment owner={comment.owner} text={comment.text} />) }
-      <hr className="post__body-separator" />
-      <input placeholder="Add a comment..." className="post__comment-input"/>
-    </div>
-  </div>
-);
